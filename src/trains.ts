eb98be125ba6b8b9887f0f5cc857c65dca8ea5b6
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2023-06-18 23:35:29 +0200

    Added eslintrc

--- src/trains.ts
+++ src/trains.ts
@@ -6,9 +6,9 @@ type lookupTrains = (lookupUrl: string) => number[];
 //TODO - implement lookupTrains (code below)
 
 export function getAllTrains(): number[] {
-	let res = new Array<number>(100);
-	for (let i = 0; i < res.length; i++) {
-		res[i] = Math.floor(Math.random() * 100000);
-	}
-	return res;
+  const res = new Array<number>(100);
+  for (let i = 0; i < res.length; i++) {
+    res[i] = Math.floor(Math.random() * 100000);
+  }
+  return res;
 }
