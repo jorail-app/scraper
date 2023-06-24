99b2b2c3187c62f89a19c78d6a5604a5626beaa2
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2023-06-24 14:22:40 +0200

    Added Prisma ORM package (#2)
    * Added prisma orm
    
    * Added service for fetching trains numbers
    
    * Changed build command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { HttpFunction } from '@google-cloud/functions-framework';
 
-import { getAllTrains } from './trains';
+import {
+  getLastCheckedTrainNumber,
+  runBackgroundTask,
+} from './services/fetchAndSaveTrainsNumbersOnce';
 
-export const main: HttpFunction = (req, res) =>
-  res.send(getAllTrains().toString());
+export const main: HttpFunction = async (_req, res) => {
+  res.send(
+    `Last checked train number is  ${(await getLastCheckedTrainNumber()) ?? 1}`,
+  );
+};
+
+runBackgroundTask()
+  .then(() => {
+    // eslint-disable-next-line no-console -- for devs
+    console.log('Finished fetching trains numbers');
+  })
+  .catch(err => {
+    console.error(err);
+  });
