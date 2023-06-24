37f6449562ef06b6793bbdeb8282ccc43fa51ccc
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2023-06-24 18:52:00 +0200

    Improvements

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,27 @@ import {
 let isBackgroundTaskRunning = false;
 
 export const main: HttpFunction = async (_req, res) => {
-  if (!isBackgroundTaskRunning) {
-    isBackgroundTaskRunning = true;
-
-    runBackgroundTask()
-      .then(() => {
-        // eslint-disable-next-line no-console -- for devs
-        console.log('Finished fetching trains numbers.');
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
-
   res.send(
-    `<pre style="overflow-wrap: break-word; word-wrap:break-word;">Checked trains numbers: ${(
-      await getCheckedTrainsNumbers()
-    ).join(', ')}`,
+    `<pre style="overflow-wrap: break-word; word-break: break-all;">Background task is ${
+      isBackgroundTaskRunning ? '' : 'not'
+    } running. Checked trains numbers: ${(await getCheckedTrainsNumbers()).join(
+      ', ',
+    )}`,
   );
 };
+
+if (!isBackgroundTaskRunning) {
+  isBackgroundTaskRunning = true;
+
+  runBackgroundTask()
+    .then(() => {
+      // eslint-disable-next-line no-console -- for devs
+      console.log('Finished fetching trains numbers.');
+    })
+    .catch(err => {
+      console.error(err);
+    })
+    .finally(() => {
+      isBackgroundTaskRunning = false;
+    });
+}
